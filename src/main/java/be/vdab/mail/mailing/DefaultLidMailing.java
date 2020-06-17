7e346b76c4668dbaef412683dfefe09e22467f9e
Nina <nina.vanhoof@vdabcampus.be>
2020-06-17 10:09:19 +0200

    async

--- src/main/java/be/vdab/mail/mailing/DefaultLidMailing.java
+++ src/main/java/be/vdab/mail/mailing/DefaultLidMailing.java
@@ -6,6 +6,7 @@ import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 import javax.mail.MessagingException;
@@ -19,6 +20,7 @@ public class DefaultLidMailing implements LidMailing {
     }
 
     @Override
+    @Async
     public void stuurMailNaRegistratie(Lid lid, String ledenURL) {
         try {
             var message = sender.createMimeMessage();
